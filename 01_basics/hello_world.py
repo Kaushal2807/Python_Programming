f802f2abe3549f1cf8c047925005e7e9bb049e9e
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-02-14 11:49:46 +0530

    Basics

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+def hello(n):
+    print(n)
+hello("Lemon")
+
+def typeofhello():
+    hello_one = "Kem cho"
+    hello_two = "su karo cho"
+    print(hello_one)
+    print(type(hello_two))
+typeofhello()
+
