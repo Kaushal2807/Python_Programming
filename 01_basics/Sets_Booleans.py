8205d593fb732ba060c694bd76501a937e35bc33
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-02-17 11:20:51 +0530

    Sets and Booleans

---
+++ 01_basics/Sets_Booleans.py
@@ -0,0 +1,19 @@
+setone = {1, 2, 3, 4, 5}
+
+print(setone & {3, 4, 5, 6})  # Intersection
+print(setone | {3, 4, 5, 6})  # Union
+
+print(setone - {3, 4, 5, 6})  # Difference
+print(type({})) #Dictionary 
+
+print(setone - {1, 2, 3, 4, 5})  # Difference
+
+# Boolean
+
+print(type(True)) #Boolean
+print(True == 1) 
+print(False == 0) 
+
+print(True is 1) #Object different hai memory location me 
+print(True)
+print(True + 4)
