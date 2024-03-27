9e15ecafa6e7f172bf90f5a6873903b0f3f21c58
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-03-27 09:38:57 +0530

    Solution 2

---
+++ 05_functions/02_Solution.py
@@ -0,0 +1,7 @@
+# Problem: Create a function that takes two numbers as parameters and returns their sum.
+a = int(input("Enter a first number:-"))
+b = int(input("Enter a second number:-"))
+
+def sum(a,b):
+    return (a + b)
+print("Sum of a and b is ",sum(a,b))
