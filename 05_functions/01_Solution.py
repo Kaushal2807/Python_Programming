63d6cffe86e5f13fc30247cc9ceebeed861724d3
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-03-27 09:38:47 +0530

    Solution 1

---
+++ 05_functions/01_Solution.py
@@ -0,0 +1,9 @@
+# Problem: Write a function to calculate and return the square of a number.
+n = int(input("Enter a number to find a square of a number: "))
+def square(n):
+    a = n**2
+    print(f"Square of a number {n} is",a)
+
+square(n)
+
+    
