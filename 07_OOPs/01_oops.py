e434a597cd9e5965ce29eb0f876c809d5bc553b5
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-04-01 21:54:05 +0530

    OOPS Concepts

--- 07_OOPs/01_oops.py
+++ 07_OOPs/01_oops.py
@@ -1,36 +1,79 @@
 # Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 
 class Car:
+    total_car = 0
+
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
+        Car.total_car += 1
 
     def get_brand(self):
-        return self.__brand + " ! "
+        return self.__brand + " !"
 
-    def fullname(self):
-        return f"{self.__brand} {self.model}"
+    def full_name(self):
+        return f"{self.__brand} {self.__model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
+    @staticmethod
+    def general_description():
+        return "Cars are means of transport"
+    
+    @property
+    def model(self):
+        return self.__model
     
 
+
 class ElectricCar(Car):
-    def __init__(self,__brand,model,battery_size):
-        super().__init__(__brand,model)
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
         self.battery_size = battery_size
 
-my_tesla = ElectricCar("Tesla","Model S","85kWh")
-print(my_tesla.model)
-print(my_tesla.fullname())
+    def fuel_type():
+        return "Electric charge"
+
+
+# my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+
+# print(isinstance(my_tesla, Car))
+# print(isinstance(my_tesla, ElectricCar))
+
 # print(my_tesla.__brand)
-print(my_tesla.get_brand())
+# print(my_tesla.fuel_type())
+
+# my_car = Car("Tata", "Safari")
+# my_car.model = "City"
+# Car("Tata", "Nexon")
 
 
-# my_car = Car("Toyota","Corolla")
+# print(my_car.general_description())
+# print(my_car.model)
+
+
+# my_car = Car("Toyota", "Corolla")
 # print(my_car.brand)
 # print(my_car.model)
-# print(my_car.fullname())
+# print(my_car.full_name())
 
-# my_new_car = Car("Tata","Safari")
-# print(my_new_car.brand)
+# my_new_car = Car("Tata", "Safari")
 # print(my_new_car.model)
-# print(my_car.fullname())
 
+
+
+class Battery:
+    def battery_info(self):
+        return "this is battery"
+
+class Engine:
+    def engine_info(self):
+        return "This is engine"
+
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+my_new_tesla = ElectricCarTwo("Tesla", "Model S")
+print(my_new_tesla.engine_info())
+print(my_new_tesla.battery_info())
